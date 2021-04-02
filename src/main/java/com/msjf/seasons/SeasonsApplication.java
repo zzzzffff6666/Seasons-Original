15546baa08cfb231fcc5cd6d459aaea3cf4b46d8
zhangfu <17770517023@163.com>
2021-04-02 21:42:52 +0800

    Initial commit

---
+++ src/main/java/com/msjf/seasons/SeasonsApplication.java
@@ -0,0 +1,13 @@
+package com.msjf.seasons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeasonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeasonsApplication.class, args);
+    }
+
+}
